65b0f02b169380ff72fab879ec0d95cbb985ca2e
HarkiratDhill <Harkirat.Dhillon@edu.sait.ca>
2025-04-18 23:02:45 -0600

    Javascript done
    I finished the main JavaScript for the website

--- main.js
+++ main.js
@@ -4,5 +4,11 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?q=Cancun&appid=24a7a6de7e
 .then(data =>{
     console.log(data);
     console.log(data.main.temp);
-    weather.textContent = data.main.temp + "\u00b0C"; 
+    console.log(data.weather[0].description);
+    console.log(data.main.humidity);
+    console.log(data.main.feels_like);
+    weather.textContent = data.main.temp + "\u00b0C";
+    type.textContent = data.weather[0].description;
+    feels.textContent = data.main.feels_like + "\u00b0C";
+    humidity.textContent = "Humidity:" + data.main.humidity;
 })
